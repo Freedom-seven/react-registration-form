baf22194bc1882c5f89dd0155e3d6be24a2dff2b
FREEDOM <freewhynane62@gmail.com>
2021-07-01 10:04:38 +0000

    add state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import './App.css';
 import image from './reCAP.png';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state={
+      email: "",
+      fullName: "",
+      phoneNumber: "",
+      kin: "",
+      fee: "",
+      fees: "",
+      location: "",
+      school: "",
+      join: "",
+      about: ""
+    };
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState({[name]:value});
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    console.log(this.state)
+  }
   render() {
     return (
-      <form className="registration">
+      <form className="registration" onSubmit={this.handleSubmit}>
         <div className="head">
         <h1> Codetrain Registration Form </h1>
         
@@ -17,73 +44,73 @@ class App extends Component {
       <div className="email">
       <label>Email address*</label>
       <br/><br/>
-      <input type="text" name="email" placeholder=" Your email"/>
+      <input type="text" name="email" onChange={this.handleChange} value={this.state.email} placeholder=" Your email"/>
       </div>
 
       <div className="name">
         <label> Full Name<span>*</span></label>
         <br/><br/>
-        <input type="text" name="fullName" id="Full Name" placeholder="Full Name"/>
+        <input type="text" name="fullName" id="Full Name" onChange={this.handleChange} value={this.state.fullName} placeholder="Full Name"/>
       </div>
 
       <div className="number">
         <label>Phone Number <span>*</span></label>
         <br/>
-        <input type="text" name="phoneNumber" placeholder="Your answer"/>
+        <input type="text" name="phoneNumber"  onChange={this.handleChange} value={this.state.phoneNumber} placeholder="Your answer"/>
       </div>
 
       <div className="kin">
         <label> Phone number of next of kin</label>
         <p>Phone number of someone we can reach out to in case your phone is off or not reachable. Eg, your parent, guardian or sibling, or spouse</p>
         <br/>
-        <input type="text" name="kin" placeholder="Your answer"/>
+        <input type="text" name="kin"  onChange={this.handleChange} value={this.state.kin} placeholder="Your answer"/>
       </div>
 
       <div className="fee">
         <p>Can you pay 60% or more of the full Codetrain fees (GHS6000 for Ghanaian applicants or $1,200 for international applicants) <span>*</span> </p>
         
-        <input type="checkbox" name="No" value="No"/>
+        <input type="checkbox" name="No"  onChange={this.handleChange} value={this.state.fee}/>
         <label>No</label>
         <br/>
         
-        <input type="checkbox" name="Yes" value="Yes"/>
+        <input type="checkbox" name="Yes"  onChange={this.handleChange} value={this.state.fee}/>
         <label>Yes</label> 
       </div>
 
       <div className="fees">
         <p>Can you pay 60% or more of the full Codetrain fees (GHS6000 for Ghanaian applicants or $1,200 for international applicants) <span>*</span></p>
-        <input type="checkbox" name="yourself" value="Yourself"/>
+        <input type="checkbox" name="yourself"  onChange={this.handleChange} value={this.state.fees}/>
         <label>Yourself</label>
         <br/>
-        <input type="checkbox" name="parentSponsor" value="Parent / Sponsor"/>
+        <input type="checkbox" name="parentSponsor"  onChange={this.handleChange} value={this.state.fees}/>
         <label>Parent / Sponser</label>
         <br/>
-        <input type="checkbox" name="organization" value="Organization"/>
+        <input type="checkbox" name="organization"  onChange={this.handleChange} value={this.state.fees}/>
         <label>Organization</label>
       </div>
 
       <div className="location">
         <label>Location <span>*</span></label>
         <br/>
-        <input type="text" name="location" placeholder="Your answer"/>
+        <input type="text" name="location"  onChange={this.handleChange} value={this.state.location} placeholder="Your answer"/>
       </div>
 
       <div className="school">
         <label> Current or previous school <span>*</span></label>
         <br/>
-        <input type="text" name="school" placeholder="Your answer"/>
+        <input type="text" name="school"  onChange={this.handleChange} value={this.state.school} placeholder="Your answer"/>
       </div><br/>
 
       <div className="join">
         <label>Why do you want to join Codetrain <span>*</span></label>
         <br/>
-        <input type="text" name="join" placeholder="Your answer"/>
+        <input type="text" name="join"  onChange={this.handleChange} value={this.state.join} placeholder="Your answer"/>
       </div><br/>
 
       <div className="about">
         <label>How did you hear about Codetrain ?<span>*</span></label>
         <br/>
-        <input type="text" name="about" placeholder="Your answer"/>
+        <input type="text" name="about"  onChange={this.handleChange} value={this.state.about} placeholder="Your answer"/>
       </div>
       <br/><br/>
 

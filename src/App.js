eb9086542fde39cec2a6752adabf8ce2a756b396
FREEDOM <freewhynane62@gmail.com>
2021-07-01 05:32:21 +0000

    add form header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        Codetrain registration form
+      <form>
+        <div className="head">
+        <h1> Codetrain Registration Form </h1>
+        
+        <p> This form is the first step to register for Codetrain's program starting on 25th July 2020. Our admissions team will reach out to you after successfully submitting your form.</p>
+        <br/>
+        <span>* Required</span>
       </div>
+      </form>
     );
   }
 }
